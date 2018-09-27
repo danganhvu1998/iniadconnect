8c38b9f9be055a8f07c60c756f2b987b921248c0
danganhvu1998 <danganhvu1998@gmail.com>
2018-09-27 18:19:31 +0900

    Finish user setting page

--- server/resources/lang/ja/messages.php
+++ server/resources/lang/ja/messages.php
@@ -5,7 +5,7 @@
 return [
     'welcome' => 'Welcome to our application',
     'dashboard' => 'Dashboard',
-    'Logout' => 'ログアウト',
+    'logout' => 'ログアウト',
     'waitAdminConfirmNewUser' => 'Please wait until admin approve your register request',
     'adminRejectedNewUser' => 'Something not right in your infomation. Please carefully check again',
     'adminBannedUser' => 'You were banned because of bad online behavior. Please do not repeat it after we allow you to continue',
@@ -15,5 +15,6 @@ return [
     'language' => "Language",
     'informationChanged' => 'Your information has successfully changed',
     'wrongPassword' => "You input password does not match our record",
+    'userInfoSetting' => "Edit Information",
 ];
 // '' => '',
--- server/resources/views/layouts/app.blade.php
+++ server/resources/views/layouts/app.blade.php
@@ -61,10 +61,13 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="/user/setting/setting">
+                                        {{ __('messages.userInfoSetting') }}
+                                    </a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                        {{ __('messages.Logout') }}
+                                        {{ __('messages.logout') }}
                                     </a>
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
